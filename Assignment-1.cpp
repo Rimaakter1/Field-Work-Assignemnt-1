1313115e52ee75c435fe89a76cd4332e5efebcc2
rimaakter1 <rimac201262@gmail.com>
2025-01-27 21:20:03 +0600

    Updated code to print Fibonacci numbers

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    for (int i = 1; i <= 10; i++) {
-        cout << i << endl;
+int main()
+{
+    int n = 10;
+    int a = 0, b = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a << endl;
+        int next = a + b;
+        a = b;
+        b = next;
     }
+
     return 0;
 }
-
